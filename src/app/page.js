a9fe034227e03d4f1326d45ee51ca43a47403a03
Gabriel Aparecido da paixão <gabriel711906@hotmail.com>
2024-09-12 19:50:08 -0300

    add home animation

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,19 @@ export default function Home() {
   return (
     <>
       <div className={styles.container}>
-        <h1>Sorvete artesanal</h1>
+        <h1>
+          <span className={styles.letter}>G</span>
+          <span className={styles.letter}>E</span>
+          <span className={styles.letter}>L</span>
+          <span className={styles.letter}>A</span>
+          <span className={styles.letter}>T</span>
+          <span className={styles.letter}>E</span> 
+          <span className={styles.letter}>R</span>
+          <span className={styles.letter}>I</span>
+          <span className={styles.letter}>A</span>
+        </h1>
       </div>
-      <Info/>
+      <Info />
     </>
   );
 }
